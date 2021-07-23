ae24c32d20b171518758297b17b931b12be9eccc
David Luna <davidluna@mail.weber.edu>
2021-07-23 03:19:24 -0600

    did 'lens slice' project

--- Projects.py
+++ Projects.py
@@ -85,3 +85,55 @@ print(gradebook)
 
 full_gradebook = last_semester_gradebook + gradebook
 print(full_gradebook)
+
+# Project 5: Len's slice
+
+# checkpoint 1
+toppings = ['pepperoni', 'pineapple', 'cheese', 'sausage', 'olives', 'anchovies', 'mushrooms']
+# print(toppings)
+
+# checkpoint 2
+prices = [2, 6, 1, 3, 2, 7, 2]
+# print(prices)
+
+#checkpoint 3
+num_two_dollar_slices = prices.count(2)
+# print(num_two_dollar_slices)
+
+#checkpoint 4
+num_pizzas = len(toppings)
+# print(num_pizzas)
+
+#checkpoint 5
+pizza_sent = 'We sell ' + str(num_pizzas)
+pizza_sent += ' different kinds of pizza.'
+# print(pizza_sent)
+
+#checkpoint 6
+pizza_and_prices = [[2, 'pepperoni'], [6, 'pineapple'], [1, 'cheese'], [3, 'sausage'], [2, 'olives'], [7, 'anchovies'], [2, 'mushrooms']]
+# print(pizza_and_prices)
+
+#checkpoint 7
+pizza_and_prices.sort()
+# print(pizza_and_prices)
+
+#checkpoint 8
+cheapest_pizza = pizza_and_prices[0][1]
+# print(cheapest_pizza)
+
+#checkpoint 9
+priciest_pizza = pizza_and_prices[-1]
+# print(priciest_pizza)
+
+#checkpoint 10
+pizza_and_prices.pop()
+print([pizza_and_prices])
+
+
+#checkpoint 11
+pizza_and_prices.insert(4, [2.5, 'peppers'])
+print(pizza_and_prices)
+
+#checkpoint 12
+three_cheapest = pizza_and_prices[:3]
+print(three_cheapest)
